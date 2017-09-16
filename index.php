628a69c1eeaa88e3b60e9e6c91de1f2de4f649ae
Kurtis Dressel <kurtisdoubled@hotmail.com>
2017-09-16 16:48:57 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+
+<header>
+	<title>Bowling FTW</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+	<link href="style.css" rel="stylesheet">
+</header>
+
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4"></div>
+			
+			<div class="col-sm-4">
+			<a href="scoreSheetForm.php" type="button" class="btn btn-primary">Play a Game</a>
+			</div>
+			
+			<div class="col-sm-4"></div>
+		</div>
+	</div>
+</body>
+
+</html>
