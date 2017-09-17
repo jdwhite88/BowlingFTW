973962e07ec6ff8e27447cae2e87ad15d9e2e03e
Jacob White <jdwhite0502@gmail.com>
2017-09-17 11:15:37 -0400

    Disallowed for 0 players

--- checkSubmit.js
+++ checkSubmit.js
@@ -2,7 +2,7 @@ function checkSubmitButton() {
     var n = document.getElementById("numPlayers").value;
     var isNum = /^\d+$/.test(n);
     var submitButton = document.getElementById("Submit");
-    if ((isNum && (n >= 0)) && (n <= 5000)) {
+    if ((isNum && (n > 0)) && (n <= 5000)) {
         submitButton.removeAttribute("disabled");
     }
     else {
