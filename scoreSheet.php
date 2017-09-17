56ad1cb948c9b715d8386324b335ff37962b2094
Kurtis Dressel <kurtisdoubled@hotmail.com>
2017-09-16 20:42:17 -0500

    added Javascript to update the table data

--- scoreSheet.php
+++ scoreSheet.php
@@ -20,9 +20,13 @@ table, th, td {
 body {
 	background-color: orange;
 }
-
 </style>
 </head>
+
+Pins <br>
+<input id="pin" type="number" name="pins">
+<button onclick="updateTable()">Add to Score</button>
+<br><br>
 
 <?php
 	$numofplayers = $_GET["numPlayers"];
@@ -30,20 +34,20 @@ body {
 
 <body>
 
-<table style="width:100%">
+<table style="width:100%" id="table">
   <tr>
     <th>Player</th>
     <th>1</th> 
     <th>2</th>
-    <th>3</th> 
+	<th>3</th> 
     <th>4</th>
-    <th>5</th> 
+	<th>5</th> 
     <th>6</th>
-    <th>7</th> 
+	<th>7</th> 
     <th>8</th>
-    <th>9</th> 
+	<th>9</th> 
     <th>10</th>
-    <th>Total</th>
+	<th>Total</th>
   </tr>
    <?php
 		
@@ -56,15 +60,21 @@ body {
 				}
 				echo"</tr>";
 			}
-			
-			echo"Pins <br>";
-			echo'<input type="text" name="pins">';
-			echo"<br><br>";
-			echo'<input type="submit">';
-			echo"</form>";
 	?>	 	
          
 </table>
+
+<script>
+function updateTable() {
+	var pins = document.getElementById("pin");
+	
+	var table = document.getElementById("table");
+	var row = table.getElementsByTagName("tr")[1];
+	var td = row.getElementsByTagName("td")[1];
+	
+	td.innerHTML = pins.value;
+}
+</script>
 
 </body>
 </html>
